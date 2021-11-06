db541a4bd7787d022716c369004a8cb5b87824f0
Shobhit Kotnala <shobhit.kotnala@gmail.com>
2021-11-06 16:10:25 +0530

    Adding code

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,20 @@
+import random
+import string
 
+def password_generator(password_length:string):
+    string_1 = string.digits
+    string_2 = string.ascii_lowercase
+    string_3 = string.ascii_uppercase
+    string_4 = string.punctuation
+
+    total_string = string_1 + string_2 + string_3 + string_4
+    my_list = []
+    my_list.extend(total_string)
+    
+    password_length = int(input(password_length))
+    return "".join(random.sample(my_list,password_length))
+    
+        
+if __name__ == "__main__":
+    password = password_generator("Please enter password length:")
+    print(f"Your password is:{password}")
